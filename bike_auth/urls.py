d3ec1b22793d9af45b44f27435d15c596c66c839
tsonko.georgiev <tsonko.georgiev@gmail.com>
2020-12-10 16:19:27 +0200

    Adding class based views

--- bike_auth/urls.py
+++ bike_auth/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from bike_auth.views import signup, login_user, logout_user, get_my_profile, update_my_profile, \
-    get_profile_of_bike_owner
+from bike_auth.views import logout_user, get_my_profile, update_my_profile, \
+    get_profile_of_bike_owner, Signup, Login
 
 urlpatterns = [
-    path('signup/', signup, name='sign up'),
-    path('login/', login_user, name='login user'),
+    # path('signup/', signup, name='sign up'),
+    path('signup/', Signup.as_view(), name='sign up'),
+    # path('login/', login_user, name='login user'),
+    path('login/', Login.as_view(), name='login user'),
     path('logout/', logout_user, name='logout user'),
     path('my-profile/<int:pk>/', get_my_profile, name='my profile'),
     path('update-my-profile/<int:pk>/', update_my_profile, name='profile update'),
--- bike_auth/views.py
+++ bike_auth/views.py
@@ -2,21 +2,51 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.models import User
 from django.db import transaction
 from django.shortcuts import render, redirect
+from django.views.generic.base import View
 
 from bike_auth.forms import SignupForm, LoginForm, ProfileForm
 from bike_auth.models import Profile
 
 
-@transaction.atomic
-def signup(request):
-    if request.method == 'GET':
+# @transaction.atomic
+# def signup(request):
+#     if request.method == 'GET':
+#         context = {
+#             'signup_form': SignupForm(),
+#             'profile_form': ProfileForm(),
+#         }
+#
+#         return render(request, 'auth/register.html', context)
+#     else:
+#         signup_form = SignupForm(request.POST)
+#         profile_form = ProfileForm(request.POST)
+#         if signup_form.is_valid() and profile_form.is_valid():
+#             user = signup_form.save()
+#             profile = profile_form.save(commit=False)
+#             profile.user = user
+#             profile_form.save()
+#
+#             login(request, user)
+#             return redirect('home page')
+#
+#         context = {
+#             'signup_form': signup_form,
+#             'profile_form': profile_form,
+#         }
+#
+#         return render(request, 'auth/register.html', context)
+
+
+class Signup(View):
+    def get(self, request):
         context = {
-            'signup_form': SignupForm(),
-            'profile_form': ProfileForm(),
-        }
+                    'signup_form': SignupForm(),
+                    'profile_form': ProfileForm(),
+                }
 
         return render(request, 'auth/register.html', context)
-    else:
+
+    def post(self, request):
         signup_form = SignupForm(request.POST)
         profile_form = ProfileForm(request.POST)
         if signup_form.is_valid() and profile_form.is_valid():
@@ -41,15 +71,44 @@ def get_next_url(params):
     return next_url if next_url != '' else 'home page'
 
 
-def login_user(request):
-    if request.method == 'GET':
+# def login_user(request):
+#     if request.method == 'GET':
+#         login_form = LoginForm()
+#         context = {
+#             'login_form': login_form,
+#         }
+#
+#         return render(request, 'auth/login.html', context)
+#     else:
+#         login_form = LoginForm(request.POST)
+#         next_url = get_next_url(request.POST)
+#
+#         if login_form.is_valid():
+#             username = login_form.cleaned_data['username']
+#             password = login_form.cleaned_data['password']
+#             user = authenticate(username=username, password=password)
+#
+#             if user:
+#                 login(request, user)
+#                 return redirect(next_url)
+#
+#         context = {
+#             'login_form': login_form,
+#         }
+#
+#         return render(request, 'auth/login.html', context)
+
+
+class Login(View):
+    def get(self, request):
         login_form = LoginForm()
         context = {
             'login_form': login_form,
         }
 
         return render(request, 'auth/login.html', context)
-    else:
+
+    def post(self, request):
         login_form = LoginForm(request.POST)
         next_url = get_next_url(request.POST)
 
